6c3a7e6488d8f10d64317a31dfc0fa32fb99d83b
AarabySelvaratnam <aarabys2000@gmail.com>
2025-05-22 19:56:28 +0530

    Update MainApp5.java

--- MainApp5.java
+++ MainApp5.java
@@ -1,56 +1,66 @@
-class Student {
-    private String name;
-    private int exam1;
-    private int exam2;
-    private int exam3;
+class Student{
+	private String name;
+	private int exam1;
+	private int exam2;
+	private int exam3;
+	
+	boolean validateMarks(int marks){
+		if (marks<=100 && marks>=0){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	Student(String na, int e1,int e2,int e3)throws Exception{
+		name=na;
+		exam1=e1;
+		exam2=e2;
+		exam3=e3;
 
-    Student(String name, int exam1, int exam2, int exam3) {
-        validateMarks(exam1);
-        validateMarks(exam2);
-        validateMarks(exam3);
-        this.name = name;
-        this.exam1 = exam1;
-        this.exam2 = exam2;
-        this.exam3 = exam3;
-    }
-
-    void validateMarks(int score) {
-        if (score >= 0 && score <= 100) {
-            
-        } else {
-            throw new IllegalArgumentException("Exam scores must be between 0 and 100");
-        }
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getExam1() {
-        return exam1;
-    }
-
-    public int getExam2() {
-        return exam2;
-    }
-
-    public int getExam3() {
-        return exam3;
-    }
-
-    public double calculateAverage() {
-        return (exam1 + exam2 + exam3) / 3.0; 
-    }
+		if (validateMarks(e1)&&validateMarks(e2)&&validateMarks(e3)){
+			exam1=e1;
+			exam2=e2;
+			exam3=e3;
+		}else {
+			System.out.println("Setting the marks value to 0");
+			throw new Exception("Invalid marks");
+		}
+		
+	}
+	
+	String getName(){
+		return name;
+	}
+	int getExam1(){
+		return exam1;
+	}
+	int getExam2(){
+		return exam2;
+	}
+	int getExam3(){
+		return exam3;
+	}
 }
 
-public class MainApp5 {
-    public static void main(String[] args) {
-        try {
-            Student st = new Student("John", 75, 110, 90); 
-            System.out.println("Student name is " + st.getName());
-            System.out.println("Average is " + st.calculateAverage());
-        } catch (IllegalArgumentException e) {
-            System.out.println("Exception: " + e.getMessage());
-        }
-    }
+class MainApp5{
+	public static void main(String[] args){
+		
+		
+		try{
+		
+		Student student2= new Student("John",75,100,90);
+		System.out.println("Name of the student 2 is : "+student2.getName());
+		System.out.println("Name of the student 2 for exam1 is : "+student2.getExam1());
+		
+		Student student1= new Student("Adam",75,110,-90);		
+		System.out.println("Name of the student 1 is : "+student1.getName());
+		System.out.println("Name of the student 1 for exam1 is : "+student1.getExam1());
+		System.out.println("Name of the student 1 for exam2 is : "+student1.getExam2());
+		
+		}catch(Exception e){
+			System.out.println("Exception is: "+e.getMessage());
+		}
+		
+	}
 }
